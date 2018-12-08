77857b4fa0ce460a5bee077476f8855bf7947ae2
Alessandro Ghedini <alessandro@ghedini.me>
2018-12-08 00:11:45 +0000

    build: abort if BoringSSL build failed

--- src/build.rs
+++ src/build.rs
@@ -6,7 +6,13 @@ fn main() {
     let bssl_dir = std::env::var("QUICHE_BSSL_PATH").unwrap_or(bssl_dir_default);
 
     if std::env::var_os("QUICHE_BSSL_PATH").is_none() {
-        Command::new("util/build_bssl.sh").status().unwrap();
+        let out = Command::new("util/build_bssl.sh").output().unwrap();
+
+        if !out.status.success() {
+            panic!("failed to build BoringSSL: \nstdout\n{}\nstderr\n{}",
+                   std::str::from_utf8(&out.stdout).unwrap(),
+                   std::str::from_utf8(&out.stderr).unwrap());
+        }
     }
 
     println!("cargo:rustc-link-search=native={}", bssl_dir);
