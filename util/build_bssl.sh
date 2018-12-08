a2069d9160f311a74c88987f2822eb466d46dbe0
Alessandro Ghedini <alessandro@ghedini.me>
2018-12-08 00:31:11 +0000

    util: don't require latest cmake version

--- util/build_bssl.sh
+++ util/build_bssl.sh
@@ -6,10 +6,12 @@ SOURCE_DIR="$PWD/deps/boringssl"
 BUILD_DIR="$OUT_DIR/boringssl"
 INSTALL_DIR="$OUT_DIR/.openssl"
 
-cmake -S $SOURCE_DIR -B $BUILD_DIR -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC"
+mkdir -p $BUILD_DIR "$INSTALL_DIR/lib"
+
+cd $BUILD_DIR
+
+cmake -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" $SOURCE_DIR
 
 make -C $BUILD_DIR VERBOSE=1 -j`nproc` bssl
-
-mkdir -p "$INSTALL_DIR/lib"
 
 cp "$BUILD_DIR/crypto/libcrypto.a" "$BUILD_DIR/ssl/libssl.a" "$INSTALL_DIR/lib/"
