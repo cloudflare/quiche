930bfc4fc90b0c29958cfe52caff85943f83f651
Alessandro Ghedini <alessandro@ghedini.me>
2019-02-08 16:51:12 +0000

    add script to automate releases

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <version>" >&2
+    exit 1
+fi
+
+if [ -n "$(git status -s)" ]; then
+    echo "Working directory is dirty."
+    exit 2
+fi
+
+VERSION=$1
+
+cargo package
+
+sed -i "/version/ s/\"\(.*\)\"/\"$VERSION\"/" Cargo.toml
+git add Cargo.toml
+git commit -m $VERSION
+git tag -a $VERSION -m "quiche $VERSION" --sign
