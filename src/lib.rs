2ac5cae2dd80a08c4f1e28bf6ab3cc3d6c69a62d
Alessandro Ghedini <alessandro@ghedini.me>
2019-04-06 15:28:21 +0100

    disable idle timer if idle_timeout transport param is zero

--- src/lib.rs
+++ src/lib.rs
@@ -1480,11 +1480,13 @@ impl Connection {
         self.pkt_num_spaces[epoch].largest_rx_pkt_num =
             cmp::max(self.pkt_num_spaces[epoch].largest_rx_pkt_num, pn);
 
-        self.idle_timer = Some(
-            now + time::Duration::from_millis(
-                self.local_transport_params.idle_timeout,
-            ),
-        );
+        if self.local_transport_params.idle_timeout > 0 {
+            self.idle_timer = Some(
+                now + time::Duration::from_millis(
+                    self.local_transport_params.idle_timeout,
+                ),
+            );
+        }
 
         self.recv_count += 1;
 
