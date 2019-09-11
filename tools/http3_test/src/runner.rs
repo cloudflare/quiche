98ca9fa4963c3954c2d330d607b3999fcaf3a326
lucas <lucas@cloudflare.com>
2019-09-11 15:10:53 +0100

    http3_tests: make idle timeout configurable in runner

--- tools/http3_test/src/runner.rs
+++ tools/http3_test/src/runner.rs
@@ -28,10 +28,9 @@ use ring::rand::*;
 
 pub fn run(
     test: &mut crate::Http3Test, peer_addr: std::net::SocketAddr,
-    verify_peer: bool,
+    verify_peer: bool, idle_timeout: u64,
 ) {
     const MAX_DATAGRAM_SIZE: usize = 1350;
-    const IDLE_TIMEOUT: u64 = 60000;
 
     let mut buf = [0; 65535];
     let mut out = [0; MAX_DATAGRAM_SIZE];
@@ -84,7 +83,7 @@ pub fn run(
         .set_application_protos(quiche::h3::APPLICATION_PROTOCOL)
         .unwrap();
 
-    config.set_idle_timeout(IDLE_TIMEOUT);
+    config.set_idle_timeout(idle_timeout);
     config.set_max_packet_size(MAX_DATAGRAM_SIZE as u64);
     config.set_initial_max_data(max_data);
     config.set_initial_max_stream_data_bidi_local(max_stream_data);
