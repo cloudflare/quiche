0cefd3675a0a2077d0bf7dea629c33d592a82a38
Alessandro Ghedini <alessandro@ghedini.me>
2019-10-01 14:29:44 +0100

    fuzz: use realistic ALPN and SCID length
    This is so we can more easily generate fuzz corpora from real
    clients/servers.

--- fuzz/src/packet_recv_server.rs
+++ fuzz/src/packet_recv_server.rs
@@ -19,7 +19,7 @@ lazy_static! {
         config.load_cert_chain_from_pem_file(&crt_path).unwrap();
         config.load_priv_key_from_pem_file(&key_path).unwrap();
         config
-            .set_application_protos(b"\x06proto1\x06proto2")
+            .set_application_protos(b"\x05hq-23\x08http/0.9")
             .unwrap();
         config.set_initial_max_data(30);
         config.set_initial_max_stream_data_bidi_local(15);
@@ -32,7 +32,7 @@ lazy_static! {
     };
 }
 
-static SCID: [u8; 16] = [0; 16];
+static SCID: [u8; quiche::MAX_CONN_ID_LEN] = [0; quiche::MAX_CONN_ID_LEN];
 
 fuzz_target!(|data: &[u8]| {
     let mut buf = data.to_vec();
