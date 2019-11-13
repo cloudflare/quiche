fe8f17ffd2e412794a820ed1e0ecf0e15f4772bd
Junho Choi <junho@cloudflare.com>
2019-11-13 13:36:21 -0800

    Build Docker images (base, qns)
    - Build a docker image for quiche artifacts (server and client)
      - moved from examples/Dockerfile to extras/docker/
    - Build a docker image for quic-interop-runner
      - imported from junhochoi/quiche-qns
    - Integration with CI
      - Build, test, publish Docker image to Docker Hub
      - Publish Docker image to Docker Hub for master branch only

---
+++ extras/docker/qns/run_endpoint.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Set up the routing needed for the simulation
+/setup.sh
+
+# The following variables are available for use:
+# - ROLE contains the role of this execution context, client or server
+# - SERVER_PARAMS contains user-supplied command line parameters
+# - CLIENT_PARAMS contains user-supplied command line parameters
+
+QUICHE_DIR=/quiche
+WWW_DIR=/www
+DOWNLOAD_DIR=/downloads
+QUICHE_CLIENT=client
+QUICHE_SERVER=server
+QUICHE_CLIENT_OPT="--no-verify --wire-version=ff000017"
+QUICHE_SERVER_OPT="--no-retry"
+
+check_testcase () {
+    TESTNAME=$1
+
+    case $1 in
+    handshake | transfer | resumption )
+        echo "supported"
+        ;;
+    retry )
+        echo "supported"
+        QUICHE_SERVER_OPT=""
+        ;;
+    http3 )
+        echo "supported"
+        QUICHE_CLIENT=http3-client
+        QUICHE_SERVER=http3-server
+        ;;
+    *)
+        echo "unsupported"
+        exit 127
+        ;;
+    esac
+}
+
+run_quiche_client_tests () {
+    for req in $REQUESTS
+    do
+        # get path only from the url
+        file=$(echo $req | perl -F'/' -an -e 'print $F[-1]')
+        RUST_LOG=${RUST_LOG:-info} $QUICHE_DIR/$QUICHE_CLIENT \
+            $QUICHE_CLIENT_OPT \
+            $CLIENT_PARAMS $req > $DOWNLOAD_DIR/$file || exit 127
+    done
+}
+
+run_quiche_server_tests() {
+    RUST_LOG=${RUST_LOG:-info} $QUICHE_DIR/$QUICHE_SERVER \
+        --listen 0.0.0.0:443 --root $WWW_DIR \
+        $SERVER_PARAMS $QUICHE_SERVER_OPT || exit 127
+}
+
+# Update config based on test case
+check_testcase $TESTCASE
+
+if [ "$ROLE" == "client" ]; then
+    # Wait for the simulator to start up.
+    /wait-for-it.sh sim:57832 -s -t 30
+    echo "## Starting quiche client..."
+    echo "## Client params: $CLIENT_PARAMS"
+    echo "## Requests: $REQUESTS"
+    echo "## Test case: $TESTCASE"
+    run_quiche_client_tests
+elif [ "$ROLE" == "server" ]; then
+    echo "## Starting quiche server..."
+    echo "## Server params: $SERVER_PARAMS"
+    echo "## Test case: $TESTCASE"
+    run_quiche_server_tests
+fi
