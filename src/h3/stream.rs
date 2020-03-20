c3ddffc037457941985f8445786e0a42bf9fe01c
lucas <lucas@cloudflare.com>
2020-03-20 13:05:42 +0000

    s/initialized/remote_initialized

--- src/h3/stream.rs
+++ src/h3/stream.rs
@@ -130,8 +130,8 @@ pub struct Stream {
     /// Whether the stream was created locally, or by the peer.
     is_local: bool,
 
-    /// Whether the stream has been initialized.
-    initialized: bool,
+    /// Whether the stream has been remotely initialized.
+    remote_initialized: bool,
 
     /// Whether the stream has been locally initialized.
     local_initialized: bool,
@@ -169,7 +169,7 @@ impl Stream {
             frame_type: None,
 
             is_local,
-            initialized: false,
+            remote_initialized: false,
             local_initialized: false,
         }
     }
@@ -190,7 +190,7 @@ impl Stream {
             Type::Push => State::PushId,
 
             Type::QpackEncoder | Type::QpackDecoder => {
-                self.initialized = true;
+                self.remote_initialized = true;
 
                 State::QpackInstruction
             },
@@ -224,10 +224,10 @@ impl Stream {
                 // Control stream starts uninitialized and only SETTINGS is
                 // accepted in that state. Other frames cause an error. Once
                 // initialized, no more SETTINGS are permitted.
-                match (ty, self.initialized) {
+                match (ty, self.remote_initialized) {
                     // Initialize control stream.
                     (frame::SETTINGS_FRAME_TYPE_ID, false) =>
-                        self.initialized = true,
+                        self.remote_initialized = true,
 
                     // Non-SETTINGS frames not allowed on control stream
                     // before initialization.
@@ -257,9 +257,9 @@ impl Stream {
                 // Request stream starts uninitialized and only HEADERS
                 // is accepted. Other frames cause an error.
                 if !self.is_local {
-                    match (ty, self.initialized) {
+                    match (ty, self.remote_initialized) {
                         (frame::HEADERS_FRAME_TYPE_ID, false) =>
-                            self.initialized = true,
+                            self.remote_initialized = true,
 
                         (frame::CANCEL_PUSH_FRAME_TYPE_ID, _) =>
                             return Err(Error::FrameUnexpected),
@@ -370,7 +370,7 @@ impl Stream {
         self.local_initialized = true
     }
 
-    /// Whether the stream has been initialized.
+    /// Whether the stream has been locally initialized.
     pub fn local_initialized(&self) -> bool {
         self.local_initialized
     }
