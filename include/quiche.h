f2a95a760b1512b9623ea061c94623785917d545
Lohith Bellad <lohith.bellad@me.com>
2021-01-14 10:44:25 -0800

    document the unit of the idle_timeout() argument

--- include/quiche.h
+++ include/quiche.h
@@ -142,7 +142,8 @@ int quiche_config_set_application_protos(quiche_config *config,
                                          const uint8_t *protos,
                                          size_t protos_len);
 
-// Sets the `max_idle_timeout` transport parameter.
+// Sets the `max_idle_timeout` transport parameter, in milliseconds, default is
+// no timeout.
 void quiche_config_set_max_idle_timeout(quiche_config *config, uint64_t v);
 
 // Sets the `max_udp_payload_size transport` parameter.
