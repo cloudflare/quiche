cloudflare/quiche

commit 77064a32f44b840efe1cdb9d53f6a2098f6c0d1f

coalesce outgoing packets
Instead of writing one QUIC packet at a time, `send()` will now try to
coalesce multiple packets onto the same UDP datagram.

In order to support that, padding of Initial packets needs to be moved
outside the QUIC packet itself, so instead of using PADDING frames, we
simply leave some free space at the end of the datagram.

One notable change, is that Initial packets from the server are now also
padded as needed to reach the minimum 1200 length. This is to ensure
that the network path actually supports the minimum size in both
directions.

Though in practice this doesn't change much, since in most cases the
packet is pretty full already so the padding done from the server is
minimal.

src/lib.rs
@@ -309,7 +309,7 @@ const MAX_ACK_RANGES: usize = 68;
 // The highest possible stream ID allowed.
 const MAX_STREAM_ID: u64 = 1 << 60;
 
-/// The default max_datagram_size used in congestion control.
+// The default max_datagram_size used in congestion control.
 const MAX_SEND_UDP_PAYLOAD_SIZE: usize = 1200;
 
 // The default length of DATAGRAM queues.
@@ -2060,6 +2060,67 @@ impl Connection {
     /// # Ok::<(), quiche::Error>(())
     /// ```
     pub fn send(&mut self, out: &mut [u8]) -> Result<usize> {
+        if out.is_empty() {
+            return Err(Error::BufferTooShort);
+        }
+
+        let mut has_initial = false;
+
+        let mut done = 0;
+
+        // Limit output packet size to respect the sender and receiver's
+        // maximum UDP payload size limit.
+        let mut left = cmp::min(out.len(), self.max_send_udp_payload_len());
+
+        // Limit data sent by the server based on the amount of data received
+        // from the client before its address is validated.
+        if !self.verified_peer_address && self.is_server {
+            left = cmp::min(left, self.max_send_bytes);
+        }
+
+        // Generate coalesced packets.
+        while left > 0 {
+            let (ty, written) =
+                match self.send_single(&mut out[done..done + left]) {
+                    Ok(v) => v,
+
+                    Err(Error::BufferTooShort) | Err(Error::Done) => break,
+
+                    Err(e) => return Err(e),
+                };
+
+            done += written;
+            left -= written;
+
+            match ty {
+                packet::Type::Initial => has_initial = true,
+
+                // No more packets can be coalesced after a 1-RTT.
+                packet::Type::Short => break,
+
+                _ => (),
+            };
+        }
+
+        if done == 0 {
+            return Err(Error::Done);
+        }
+
+        // Pad UDP datagram if it contains a QUIC Initial packet.
+        if has_initial && left > 0 && done < MIN_CLIENT_INITIAL_LEN {
+            let pad_len = cmp::min(left, MIN_CLIENT_INITIAL_LEN - done);
+
+            // Fill padding area with null bytes, to avoid leaking information
+            // in case the application reuses the packet buffer.
+            out[done..done + pad_len].fill(0);
+
+            done += pad_len;
+        }
+
+        Ok(done)
+    }
+
+    fn send_single(&mut self, out: &mut [u8]) -> Result<(packet::Type, usize)> {
         let now = time::Instant::now();
 
         if out.is_empty() {
@@ -2166,18 +2227,8 @@ impl Connection {
 
         let mut left = b.cap();
 
-        // Limit output packet size to respect the sender and receiver's
-        // maximum UDP payload size limit.
-        left = cmp::min(left, self.max_send_udp_payload_len());
-
         // Limit output packet size by congestion window size.
         left = cmp::min(left, self.recovery.cwnd_available());
-
-        // Limit data sent by the server based on the amount of data received
-        // from the client before its address is validated.
-        if !self.verified_peer_address && self.is_server {
-            left = cmp::min(left, self.max_send_bytes);
-        }
 
         let pn = self.pkt_num_spaces[epoch].next_pkt_num;
         let pn_len = packet::pkt_num_len(pn)?;
@@ -2695,20 +2746,6 @@ impl Connection {
             return Err(Error::Done);
         }
 
-        // Pad the client's initial packet.
-        if !self.is_server && pkt_type == packet::Type::Initial {
-            let payload_len = b.off() - payload_offset;
-            let pkt_len = pn_len + payload_len + crypto_overhead;
-
-            let frame = frame::Frame::Padding {
-                len: cmp::min(MIN_CLIENT_INITIAL_LEN - pkt_len, left),
-            };
-
-            if push_frame_to_pkt!(b, frames, frame, left) {
-                in_flight = true;
-            }
-        }
-
         // Pad payload so that it's always at least 4 bytes.
         if b.off() - payload_offset < PAYLOAD_MIN_LEN {
             let payload_len = b.off() - payload_offset;
@@ -2850,7 +2887,7 @@ impl Connection {
             self.ack_eliciting_sent = true;
         }
 
-        Ok(written)
+        Ok((pkt_type, written))
     }
 
     // Returns the maximum size of a packet to be sent.
@@ -5413,7 +5450,7 @@ mod tests {
         let flight = testing::emit_flight(&mut pipe.server).unwrap();
         let server_sent = flight.iter().fold(0, |out, p| out + p.len());
 
-        assert_eq!(server_sent, (client_sent - 1) * MAX_AMPLIFICATION_FACTOR);
+        assert_eq!(server_sent, (client_sent - 2) * MAX_AMPLIFICATION_FACTOR);
     }
 
     #[test]
@@ -8066,20 +8103,16 @@ mod tests {
         testing::process_flight(&mut pipe.client, flight).unwrap();
 
         // Client sends Initial packet with ACK.
-        let len = pipe.client.send(&mut buf).unwrap();
-
-        let hdr = Header::from_slice(&mut buf[..len], 0).unwrap();
-        assert_eq!(hdr.ty, Type::Initial);
+        let (ty, len) = pipe.client.send_single(&mut buf).unwrap();
+        assert_eq!(ty, Type::Initial);
 
         assert_eq!(pipe.server.recv(&mut buf[..len]), Ok(len));
 
         // Client sends Handshake packet.
-        let len = pipe.client.send(&mut buf).unwrap();
+        let (ty, len) = pipe.client.send_single(&mut buf).unwrap();
+        assert_eq!(ty, Type::Handshake);
 
-        let hdr = Header::from_slice(&mut buf[..len], 0).unwrap();
-        assert_eq!(hdr.ty, Type::Handshake);
-
-        // Packet type is corrupted to Initial..
+        // Packet type is corrupted to Initial.
         buf[0] &= !(0x20);
 
         let hdr = Header::from_slice(&mut buf[..len], 0).unwrap();

commit 03afbbd400950ac584e2ae001a06c754da75ee11

avoid coalescing probes of the same type
Follow-up to 77064a32, which caused multiple probe packets of the same
type to be coalesced together.

src/lib.rs
@@ -2141,6 +2141,14 @@ impl Connection {
 
                 _ => (),
             };
+
+            // When sending multiple PTO probes, don't coalesce them together,
+            // so they are sent on separate UDP datagrams.
+            if let Ok(epoch) = ty.to_epoch() {
+                if self.recovery.loss_probes[epoch] > 0 {
+                    break;
+                }
+            }
         }
 
         if done == 0 {
@@ -8470,6 +8478,46 @@ mod tests {
                 data: stream::RangeBuf::from(b"b", 0, false),
             })
         );
+    }
+
+    #[test]
+    /// Tests that PTO probe packets are not coalesced together.
+    fn dont_coalesce_probes() {
+        let mut buf = [0; 65535];
+
+        let mut pipe = testing::Pipe::default().unwrap();
+
+        // Client send Initial packet.
+        assert_eq!(pipe.client.send(&mut buf), Ok(1200));
+
+        // Wait for PTO to expire.
+        let timer = pipe.client.timeout().unwrap();
+        std::thread::sleep(timer + time::Duration::from_millis(1));
+
+        pipe.client.on_timeout();
+
+        let epoch = packet::EPOCH_INITIAL;
+        assert_eq!(pipe.client.recovery.loss_probes[epoch], 1);
+
+        // Client sends PTO probe.
+        assert_eq!(pipe.client.send(&mut buf), Ok(1200));
+        assert_eq!(pipe.client.recovery.loss_probes[epoch], 0);
+
+        // Wait for PTO to expire.
+        let timer = pipe.client.timeout().unwrap();
+        std::thread::sleep(timer + time::Duration::from_millis(1));
+
+        pipe.client.on_timeout();
+
+        assert_eq!(pipe.client.recovery.loss_probes[epoch], 2);
+
+        // Client sends first PTO probe.
+        assert_eq!(pipe.client.send(&mut buf), Ok(1200));
+        assert_eq!(pipe.client.recovery.loss_probes[epoch], 1);
+
+        // Client sends second PTO probe.
+        assert_eq!(pipe.client.send(&mut buf), Ok(1200));
+        assert_eq!(pipe.client.recovery.loss_probes[epoch], 0);
     }
 
     #[test]