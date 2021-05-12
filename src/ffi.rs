46d19bcd02699753a56ec061e2b2dc96f377be68
Norman Maurer <norman_maurer@apple.com>
2021-05-12 20:23:04 +0200

    Expose connection source / destination id via c API
    Motivation:
    
    We should expose functions to access the source and destination ids via the c API as well
    
    Modifications:
    
    Expose functions in the C API
    
    Result:
    
    Be able to access the ids of a connection

--- src/ffi.rs
+++ src/ffi.rs
@@ -767,6 +767,26 @@ pub extern fn quiche_conn_trace_id(
 }
 
 #[no_mangle]
+pub extern fn quiche_conn_source_id(
+    conn: &mut Connection, out: &mut *const u8, out_len: &mut size_t,
+) {
+    let id = conn.source_id().to_vec();
+
+    *out = id.as_ptr();
+    *out_len = id.len();
+}
+
+#[no_mangle]
+pub extern fn quiche_conn_destination_id(
+    conn: &mut Connection, out: &mut *const u8, out_len: &mut size_t,
+) {
+    let id = conn.destination_id().to_vec();
+
+    *out = id.as_ptr();
+    *out_len = id.len();
+}
+
+#[no_mangle]
 pub extern fn quiche_conn_application_proto(
     conn: &mut Connection, out: &mut *const u8, out_len: &mut size_t,
 ) {
