437f5a6eee23cc76f76bdc19668f8fc6e5cd4d90
Miguel Guarniz <mi9uel9@gmail.com>
2022-06-21 18:42:41 -0400

    add docs
    Signed-off-by: Miguel Guarniz <mi9uel9@gmail.com>

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -1128,7 +1128,7 @@ impl Config {
         self.disable_dcid_reuse = v;
     }
 
-    // TODO: add docs
+    /// Set the server's preferred address.
     pub fn set_preferred_address(
         &mut self,
         addr_str_v4: Option<String>,
@@ -5784,7 +5784,7 @@ impl Connection {
         }
     }
 
-    // TODO: add docs
+    /// Returns the server's preferred address for V4.
     pub fn server_preferred_address_v4(&self) -> Option<(SocketAddrV4, ConnectionId, Vec<u8>)> {
         match self.peer_transport_params.preferred_address_params.as_ref() {
             Some(params) => params.address_v4(),
@@ -5792,7 +5792,7 @@ impl Connection {
         }
     }
 
-    // TODO: add docs
+    /// Returns the server's preferred address for V6.
     pub fn server_preferred_address_v6(&self) -> Option<(SocketAddrV6, ConnectionId, Vec<u8>)> {
         match self.peer_transport_params.preferred_address_params.as_ref() {
             Some(params) => params.address_v6(),
@@ -5916,6 +5916,7 @@ impl Connection {
             .set_source_conn_id_limit(peer_params.active_conn_id_limit);
 
         if let Some(preferred_address) = &peer_params.preferred_address_params {
+            // Zero-length connection ID must not be sent.
             if preferred_address.connection_id.len() == 0 {
                 return Err(Error::InvalidTransportParam);
             }
@@ -7542,7 +7543,6 @@ impl TransportParams {
     }
 }
 
-// TODO: add docs
 #[derive(Clone, Debug, PartialEq)]
 struct PreferredAddressParams<'a> {
     addr_v4: Option<SocketAddrV4>,
