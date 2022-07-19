b841ca548a3b9fcb14c1ae16aa8b22b683a3646d
msoxzw <56633971+msoxzw@users.noreply.github.com>
2022-07-19 08:49:20 +0000

    build: use GNU ABI and fix linking order to build with the MinGW toolchain

--- quiche/src/build.rs
+++ quiche/src/build.rs
@@ -32,9 +32,7 @@ const CMAKE_PARAMS_ARM_LINUX: &[(&str, &[(&str, &str)])] = &[
 /// so adjust library location based on platform and build target.
 /// See issue: https://github.com/alexcrichton/cmake-rs/issues/18
 fn get_boringssl_platform_output_path() -> String {
-    let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
-
-    if target_env == "msvc" {
+    if cfg!(target_env = "msvc") {
         // Code under this branch should match the logic in cmake-rs
         let debug_env_var =
             std::env::var("DEBUG").expect("DEBUG variable not defined in env");
@@ -157,18 +155,6 @@ fn get_boringssl_cmake_config() -> cmake::Config {
             _ => boringssl_cmake,
         },
 
-        "windows" => match arch.as_ref() {
-            "x86_64" => {
-                // Override _WIN32_WINNT to use 64bit APIs,
-                // such as GetTickCount64().
-                boringssl_cmake.cxxflag("-D_WIN32_WINNT=0x0600");
-
-                boringssl_cmake
-            },
-
-            _ => boringssl_cmake,
-        },
-
         _ => {
             // Configure BoringSSL for building on 32-bit non-windows platforms.
             if arch == "x86" && os != "windows" {
@@ -242,7 +228,8 @@ fn main() {
                     .cxxflag("-DBORINGSSL_UNSAFE_FUZZER_MODE");
             }
 
-            cfg.build_target("bssl").build().display().to_string()
+            cfg.build_target("ssl").build();
+            cfg.build_target("crypto").build().display().to_string()
         });
 
         let build_path = get_boringssl_platform_output_path();
