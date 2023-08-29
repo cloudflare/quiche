ef93fe977866d3b8b1ef3def6916e57ec39a6b0c
Alessandro Ghedini <alessandro@ghedini.me>
2023-08-29 08:11:12 +0100

    apps: update to nix 0.27
    One notable change is that functions like `setsockopt()` now require
    `AsFd` instead of the raw fd, but mio doesn't implement that, so we need
    to convert fds explicitly.

--- apps/src/sendto.rs
+++ apps/src/sendto.rs
@@ -35,7 +35,10 @@ pub fn detect_gso(socket: &mio::net::UdpSocket, segment_size: usize) -> bool {
     use nix::sys::socket::sockopt::UdpGsoSegment;
     use std::os::unix::io::AsRawFd;
 
-    setsockopt(socket.as_raw_fd(), UdpGsoSegment, &(segment_size as i32)).is_ok()
+    // mio::net::UdpSocket doesn't implement AsFd (yet?).
+    let fd = unsafe { std::os::fd::BorrowedFd::borrow_raw(socket.as_raw_fd()) };
+
+    setsockopt(&fd, UdpGsoSegment, &(segment_size as i32)).is_ok()
 }
 
 /// For non-Linux, there is no GSO support.
