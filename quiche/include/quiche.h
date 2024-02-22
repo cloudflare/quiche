3630e4455ea7933de305d7a05687a54e7ed039b7
Bob Weinand <bobwei9@hotmail.com>
2024-02-22 23:13:06 +0100

    Expose stream application error in FFI
    Fixes #1699

--- quiche/include/quiche.h
+++ quiche/include/quiche.h
@@ -379,11 +379,13 @@ size_t quiche_conn_send_quantum_on_path(const quiche_conn *conn,
 
 // Reads contiguous data from a stream.
 ssize_t quiche_conn_stream_recv(quiche_conn *conn, uint64_t stream_id,
-                                uint8_t *out, size_t buf_len, bool *fin);
+                                uint8_t *out, size_t buf_len, bool *fin,
+                                uint64_t *application_error);
 
 // Writes data to a stream.
 ssize_t quiche_conn_stream_send(quiche_conn *conn, uint64_t stream_id,
-                                const uint8_t *buf, size_t buf_len, bool fin);
+                                const uint8_t *buf, size_t buf_len, bool fin,
+                                uint64_t application_error);
 
 // The side of the stream to be shut down.
 enum quiche_shutdown {
