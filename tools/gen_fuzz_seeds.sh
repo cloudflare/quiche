6ca3a4ecf196bf31f67f19d81f6b8b044551d5c3
Alessandro Ghedini <alessandro@ghedini.me>
2024-04-05 12:29:55 +0100

    add script to generate client/server fuzzing seeds

---
+++ tools/gen_fuzz_seeds.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+cleanup() {
+    echo "Cleaning up..."
+    rm -r $CLIENT_DIR $SERVER_DIR
+    kill %1
+}
+
+trap cleanup EXIT
+
+CLIENT_DIR=$(mktemp -d)
+SERVER_DIR=$(mktemp -d)
+
+# Build apps first.
+cargo build --features fuzzing -p quiche_apps
+
+# Run server in the background.
+target/debug/quiche-server --cert fuzz/cert.crt --key fuzz/cert.key --no-retry --dump-packets $SERVER_DIR &
+
+# Wait for server to be ready.
+sleep 1
+
+# Run client.
+RUST_LOG=trace target/debug/quiche-client --no-verify https://127.0.0.1:4433 --dump-packets $CLIENT_DIR
+
+# Combine client-received packets into client's seed.
+cat $CLIENT_DIR/*.pkt > fuzz/corpus/packet_recv_client/seed
+
+# Combine server-received packets into client's seed.
+cat $SERVER_DIR/*.pkt > fuzz/corpus/packet_recv_server/seed
+
+# Minimize fuzz corpora.
+cargo +nightly fuzz cmin -Oa packet_recv_client
+cargo +nightly fuzz cmin -Oa packet_recv_server
