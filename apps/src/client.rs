74ade4aad279566f689c5ddc5c37e0a048ceae25
Naveen Nathan <naveen@lastninja.net>
2024-09-28 21:28:14 +1000

    apps: implement mutual tls with optional strict authentication

--- apps/src/client.rs
+++ apps/src/client.rs
@@ -99,7 +99,15 @@ pub fn connect(
     };
 
     // Create the configuration for the QUIC connection.
-    let mut config = quiche::Config::new(args.version).unwrap();
+    let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION).unwrap();
+
+    if let Some(cert) = args.cert {
+        config.load_cert_chain_from_pem_file(&cert).unwrap();
+    }
+
+    if let Some(key) = args.key {
+        config.load_priv_key_from_pem_file(&key).unwrap();
+    }
 
     if let Some(ref trust_origin_ca_pem) = args.trust_origin_ca_pem {
         config
