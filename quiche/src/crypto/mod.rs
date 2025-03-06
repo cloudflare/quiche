cloudflare/quiche

commit b17904e522777746b2a6b12667d85283e1df368c

crypto: use the FIPS-compliant AEAD from BoringCrypto
The main difference is that the _tls13() AEAD follows the FIPS
requirements, the main one being that AEAD counters are strictly
monotonically incresing for each seal operation.

quiche/src/crypto/mod.rs
@@ -238,7 +238,7 @@ impl Seal {
     }
 
     pub fn from_secret(aead: Algorithm, secret: &[u8]) -> Result<Seal> {
-        Ok(Seal {
+        let seal = Seal {
             alg: aead,
 
             secret: secret.to_vec(),
@@ -246,7 +246,17 @@ impl Seal {
             header: HeaderProtectionKey::from_secret(aead, secret)?,
 
             packet: PacketKey::from_secret(aead, secret, Self::ENCRYPT)?,
-        })
+        };
+
+        // Dummy seal operation to prime the AEAD context with the nonce mask.
+        //
+        // This is needed because BoringCrypto requires the first counter (i.e.
+        // packet number) to be zero, which would not be the case for packet
+        // number spaces after Initial as the same packet number sequence is
+        // shared.
+        let _ = seal.seal_with_u64_counter(0, b"", &mut [0_u8; 16], 0, None);
+
+        Ok(seal)
     }
 
     pub fn new_mask(&self, sample: &[u8]) -> Result<[u8; 5]> {

quiche/src/crypto/boringssl.rs
@@ -18,8 +18,8 @@ struct EVP_AEAD_CTX {
 impl Algorithm {
     fn get_evp_aead(self) -> *const EVP_AEAD {
         match self {
-            Algorithm::AES128_GCM => unsafe { EVP_aead_aes_128_gcm() },
-            Algorithm::AES256_GCM => unsafe { EVP_aead_aes_256_gcm() },
+            Algorithm::AES128_GCM => unsafe { EVP_aead_aes_128_gcm_tls13() },
+            Algorithm::AES256_GCM => unsafe { EVP_aead_aes_256_gcm_tls13() },
             Algorithm::ChaCha20_Poly1305 => unsafe {
                 EVP_aead_chacha20_poly1305()
             },
@@ -227,9 +227,9 @@ pub(crate) fn hkdf_expand(
 }
 
 extern {
-    fn EVP_aead_aes_128_gcm() -> *const EVP_AEAD;
+    fn EVP_aead_aes_128_gcm_tls13() -> *const EVP_AEAD;
 
-    fn EVP_aead_aes_256_gcm() -> *const EVP_AEAD;
+    fn EVP_aead_aes_256_gcm_tls13() -> *const EVP_AEAD;
 
     fn EVP_aead_chacha20_poly1305() -> *const EVP_AEAD;
 

commit 35a7e8c9e7e812ebef5cb268238872b5c89a85d3

crypto/boringssl: perform dummy seal when client resets keys too
This is yet more fallout from b17904e5 (also see 7d686e1). This time the
missing dummy seal is for clients, after the initial epoch is reset
(e.g. due to retry or version negotiation), which also causes the
initial key material to be dervied again.

When that happens, and if the client's PTO fires (e.g. because the
server is slow to respond) that will trigger the client to create a new
Initial packet that will fail to seal because the initial seal context
expects a packet number of 0, while the actual packet number is greater
than that as it's a retransmission.

This only happens when the client's initial PTO fires because otherwise
it would just send a Handshake packet instead, without using the initial
key material.

quiche/src/crypto/mod.rs
@@ -311,7 +311,7 @@ impl HeaderProtectionKey {
 }
 
 pub fn derive_initial_key_material(
-    cid: &[u8], version: u32, is_server: bool,
+    cid: &[u8], version: u32, is_server: bool, did_reset: bool,
 ) -> Result<(Open, Seal)> {
     let mut initial_secret = [0; 32];
     let mut client_secret = vec![0; 32];
@@ -324,12 +324,34 @@ pub fn derive_initial_key_material(
 
     derive_initial_secret(cid, version, &mut initial_secret)?;
 
+    derive_client_initial_secret(aead, &initial_secret, &mut client_secret)?;
+
+    derive_server_initial_secret(aead, &initial_secret, &mut server_secret)?;
+
+    // When the initial key material has been reset (e.g. due to retry or
+    // version negotiation), we need to prime the AEAD context as well, as the
+    // following packet will not start from 0 again. This is done through the
+    // `Open/Seal::from_secret()` path, rather than `Open/Seal::new()`.
+    if did_reset {
+        let (open, seal) = if is_server {
+            (
+                Open::from_secret(aead, &client_secret)?,
+                Seal::from_secret(aead, &server_secret)?,
+            )
+        } else {
+            (
+                Open::from_secret(aead, &server_secret)?,
+                Seal::from_secret(aead, &client_secret)?,
+            )
+        };
+
+        return Ok((open, seal));
+    }
+
     // Client.
     let mut client_key = vec![0; key_len];
     let mut client_iv = vec![0; nonce_len];
     let mut client_hp_key = vec![0; key_len];
-
-    derive_client_initial_secret(aead, &initial_secret, &mut client_secret)?;
 
     derive_pkt_key(aead, &client_secret, &mut client_key)?;
     derive_pkt_iv(aead, &client_secret, &mut client_iv)?;
@@ -339,8 +361,6 @@ pub fn derive_initial_key_material(
     let mut server_key = vec![0; key_len];
     let mut server_iv = vec![0; nonce_len];
     let mut server_hp_key = vec![0; key_len];
-
-    derive_server_initial_secret(aead, &initial_secret, &mut server_secret)?;
 
     derive_pkt_key(aead, &server_secret, &mut server_key)?;
     derive_pkt_iv(aead, &server_secret, &mut server_iv)?;

quiche/src/lib.rs
@@ -2076,6 +2076,7 @@ impl Connection {
                 &dcid,
                 conn.version,
                 conn.is_server,
+                false,
             )?;
 
             let reset_token = conn.peer_transport_params.stateless_reset_token;
@@ -2531,6 +2532,7 @@ impl Connection {
                 &self.destination_id(),
                 self.version,
                 self.is_server,
+                true,
             )?;
 
             // Reset connection state to force sending another Initial packet.
@@ -2596,6 +2598,7 @@ impl Connection {
                 &hdr.scid,
                 self.version,
                 self.is_server,
+                true,
             )?;
 
             // Reset connection state to force sending another Initial packet.
@@ -2665,6 +2668,7 @@ impl Connection {
                 &hdr.dcid,
                 self.version,
                 self.is_server,
+                false,
             )?;
 
             self.pkt_num_spaces[packet::Epoch::Initial].crypto_open =
@@ -13056,22 +13060,92 @@ mod tests {
         // Client receives Retry and sends new Initial.
         assert_eq!(pipe.client_recv(&mut buf[..len]), Ok(len));
 
-        let (len, _) = pipe.client.send(&mut buf).unwrap();
+        let (len, send_info) = pipe.client.send(&mut buf).unwrap();
 
         let hdr = Header::from_slice(&mut buf[..len], MAX_CONN_ID_LEN).unwrap();
         assert_eq!(&hdr.token.unwrap(), token);
 
         // Server accepts connection.
-        let from = "127.0.0.1:1234".parse().unwrap();
         pipe.server = accept(
             &scid,
             Some(&odcid),
             testing::Pipe::server_addr(),
-            from,
+            send_info.from,
             &mut config,
         )
         .unwrap();
         assert_eq!(pipe.server_recv(&mut buf[..len]), Ok(len));
+
+        assert_eq!(pipe.advance(), Ok(()));
+
+        assert!(pipe.client.is_established());
+        assert!(pipe.server.is_established());
+    }
+
+    #[test]
+    fn retry_with_pto() {
+        let mut buf = [0; 65535];
+
+        let mut config = Config::new(PROTOCOL_VERSION).unwrap();
+        config
+            .load_cert_chain_from_pem_file("examples/cert.crt")
+            .unwrap();
+        config
+            .load_priv_key_from_pem_file("examples/cert.key")
+            .unwrap();
+        config
+            .set_application_protos(&[b"proto1", b"proto2"])
+            .unwrap();
+
+        let mut pipe = testing::Pipe::with_server_config(&mut config).unwrap();
+
+        // Client sends initial flight.
+        let (mut len, _) = pipe.client.send(&mut buf).unwrap();
+
+        // Server sends Retry packet.
+        let hdr = Header::from_slice(&mut buf[..len], MAX_CONN_ID_LEN).unwrap();
+
+        let odcid = hdr.dcid.clone();
+
+        let mut scid = [0; MAX_CONN_ID_LEN];
+        rand::rand_bytes(&mut scid[..]);
+        let scid = ConnectionId::from_ref(&scid);
+
+        let token = b"quiche test retry token";
+
+        len = packet::retry(
+            &hdr.scid,
+            &hdr.dcid,
+            &scid,
+            token,
+            hdr.version,
+            &mut buf,
+        )
+        .unwrap();
+
+        // Client receives Retry and sends new Initial.
+        assert_eq!(pipe.client_recv(&mut buf[..len]), Ok(len));
+
+        let (len, send_info) = pipe.client.send(&mut buf).unwrap();
+
+        let hdr = Header::from_slice(&mut buf[..len], MAX_CONN_ID_LEN).unwrap();
+        assert_eq!(&hdr.token.unwrap(), token);
+
+        // Server accepts connection.
+        pipe.server = accept(
+            &scid,
+            Some(&odcid),
+            testing::Pipe::server_addr(),
+            send_info.from,
+            &mut config,
+        )
+        .unwrap();
+        assert_eq!(pipe.server_recv(&mut buf[..len]), Ok(len));
+
+        // Wait for the client's PTO so it will try to send an Initial again.
+        let timer = pipe.client.timeout().unwrap();
+        std::thread::sleep(timer + time::Duration::from_millis(1));
+        pipe.client.on_timeout();
 
         assert_eq!(pipe.advance(), Ok(()));
 

quiche/src/packet.rs
@@ -1312,9 +1312,13 @@ mod tests {
 
         let payload_len = b.get_varint().unwrap() as usize;
 
-        let (aead, _) =
-            crypto::derive_initial_key_material(dcid, hdr.version, is_server)
-                .unwrap();
+        let (aead, _) = crypto::derive_initial_key_material(
+            dcid,
+            hdr.version,
+            is_server,
+            false,
+        )
+        .unwrap();
 
         decrypt_hdr(&mut b, &mut hdr, &aead).unwrap();
         assert_eq!(hdr.pkt_num_len, expected_pn_len);
@@ -1561,9 +1565,13 @@ mod tests {
 
         b.put_bytes(header).unwrap();
 
-        let (_, aead) =
-            crypto::derive_initial_key_material(dcid, hdr.version, is_server)
-                .unwrap();
+        let (_, aead) = crypto::derive_initial_key_material(
+            dcid,
+            hdr.version,
+            is_server,
+            false,
+        )
+        .unwrap();
 
         let payload_len = frames.len();
 
@@ -1947,7 +1955,8 @@ mod tests {
         let payload_len = b.get_varint().unwrap() as usize;
 
         let (aead, _) =
-            crypto::derive_initial_key_material(b"", hdr.version, true).unwrap();
+            crypto::derive_initial_key_material(b"", hdr.version, true, false)
+                .unwrap();
 
         assert_eq!(
             decrypt_pkt(&mut b, 0, 1, payload_len, &aead),
@@ -1980,7 +1989,8 @@ mod tests {
         let payload_len = 1;
 
         let (aead, _) =
-            crypto::derive_initial_key_material(b"", hdr.version, true).unwrap();
+            crypto::derive_initial_key_material(b"", hdr.version, true, false)
+                .unwrap();
 
         assert_eq!(
             decrypt_pkt(&mut b, 0, 1, payload_len, &aead),