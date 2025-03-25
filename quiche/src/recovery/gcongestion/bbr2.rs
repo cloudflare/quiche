55089a580d9e2013a3c01e4decfa2b124b641c4e
Antonio Vicente <avicente@cloudflare.com>
2025-03-25 11:00:16 -0700

    Add BBRv2 network_model to gcongestion subdirectory.
    Library has no tests.

--- quiche/src/recovery/gcongestion/bbr2.rs
+++ quiche/src/recovery/gcongestion/bbr2.rs
@@ -28,6 +28,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+mod network_model;
+
 use std::time::Duration;
 use std::time::Instant;
 
