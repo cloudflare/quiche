4a9973012c92ebeeebc388172a30fa0bb7b10cef
Antonio Vicente <avicente@cloudflare.com>
2025-03-27 16:32:06 -0700

    fix dead_code warning

--- quiche/src/recovery/gcongestion/bbr/bandwidth_sampler.rs
+++ quiche/src/recovery/gcongestion/bbr/bandwidth_sampler.rs
@@ -884,14 +884,9 @@ mod bandwidth_sampler_tests {
         }
 
         fn make_acked_packet(&self, pkt_num: u64) -> Acked {
-            let size = self.get_packet_size(pkt_num);
             let time_sent = self.get_packet_time(pkt_num);
 
-            Acked {
-                pkt_num,
-                time_sent,
-                size,
-            }
+            Acked { pkt_num, time_sent }
         }
 
         fn make_lost_packet(&self, pkt_num: u64) -> Lost {
@@ -953,8 +948,10 @@ mod bandwidth_sampler_tests {
             let acked = acked
                 .into_iter()
                 .map(|pkt| {
+                    let acked_size = self.get_packet_size(*pkt);
+                    self.bytes_in_flight -= acked_size;
+
                     let acked = self.make_acked_packet(*pkt);
-                    self.bytes_in_flight -= acked.size;
                     acked
                 })
                 .collect::<Vec<_>>();
@@ -1468,7 +1465,6 @@ mod bandwidth_sampler_tests {
         let acked = Acked {
             pkt_num: 1,
             time_sent: test_sender.clock,
-            size: REGULAR_PACKET_SIZE,
         };
         test_sender.advance_time(Duration::from_millis(10));
         let sample = test_sender.sampler.on_congestion_event(
