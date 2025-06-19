hfyeomans/conceal-quiche

Annotate qlog on congestion control state transitions.
Based on https://github.com/cloudflare/quiche/pull/1357, but extended to cover
BBR2 and HyStart++

quiche/src/recovery/congestion/bbr2/mod.rs
@@ -45,6 +45,7 @@ pub(crate) static BBR2: CongestionControlOps = CongestionControlOps {
     checkpoint,
     rollback,
     has_custom_pacing,
+    state_str,
     debug_fmt,
 };
 
@@ -144,6 +145,20 @@ enum BBR2StateMachine {
     ProbeBWREFILL,
     ProbeBWUP,
     ProbeRTT,
+}
+
+impl From<BBR2StateMachine> for &'static str {
+    fn from(state: BBR2StateMachine) -> &'static str {
+        match state {
+            BBR2StateMachine::Startup => "bbr2_startup",
+            BBR2StateMachine::Drain => "bbr2_drain",
+            BBR2StateMachine::ProbeBWDOWN => "bbr2_probe_bw_down",
+            BBR2StateMachine::ProbeBWCRUISE => "bbr2_probe_bw_cruise",
+            BBR2StateMachine::ProbeBWREFILL => "bbr2_probe_bw_refill",
+            BBR2StateMachine::ProbeBWUP => "bbr2_probe_bw_up",
+            BBR2StateMachine::ProbeRTT => "bbr2_probe_rtt",
+        }
+    }
 }
 
 /// BBR2 Ack Phases.
@@ -618,6 +633,10 @@ fn rate_kbps(rate: u64) -> isize {
     } else {
         (rate * 8 / 1000) as isize
     }
+}
+
+fn state_str(r: &Congestion, _now: Instant) -> &'static str {
+    r.bbr2_state.state.into()
 }
 
 fn debug_fmt(r: &Congestion, f: &mut std::fmt::Formatter) -> std::fmt::Result {

quiche/src/recovery/gcongestion/bbr2/probe_bw.rs
@@ -62,6 +62,16 @@ enum AdaptUpperBoundsResult {
 }
 
 impl ModeImpl for ProbeBW {
+    fn state_str(&self) -> &'static str {
+        match self.cycle.phase {
+            CyclePhase::NotStarted => unreachable!(),
+            CyclePhase::Up => "bbr2_probe_bw_up",
+            CyclePhase::Down => "bbr2_probe_bw_down",
+            CyclePhase::Cruise => "bbr2_probe_bw_cruise",
+            CyclePhase::Refill => "bbr2_probe_bw_refill",
+        }
+    }
+
     fn enter(
         &mut self, now: Instant,
         _congestion_event: Option<&BBRv2CongestionEvent>, params: &Params,

cloudflare/quiche

quiche/src/recovery/congestion/bbr2/mod.rs
@@ -42,6 +42,7 @@ pub(crate) static BBR2: CongestionControlOps = CongestionControlOps {
     checkpoint,
     rollback,
     has_custom_pacing,
+    state_str,
     debug_fmt,
 };
 
@@ -141,6 +142,20 @@ enum BBR2StateMachine {
     ProbeBWREFILL,
     ProbeBWUP,
     ProbeRTT,
+}
+
+impl From<BBR2StateMachine> for &'static str {
+    fn from(state: BBR2StateMachine) -> &'static str {
+        match state {
+            BBR2StateMachine::Startup => "bbr_startup",
+            BBR2StateMachine::Drain => "bbr_drain",
+            BBR2StateMachine::ProbeBWDOWN => "bbr_probe_bw_down",
+            BBR2StateMachine::ProbeBWCRUISE => "bbr_probe_bw_cruise",
+            BBR2StateMachine::ProbeBWREFILL => "bbr_probe_bw_refill",
+            BBR2StateMachine::ProbeBWUP => "bbr_probe_bw_up",
+            BBR2StateMachine::ProbeRTT => "bbr_probe_rtt",
+        }
+    }
 }
 
 /// BBR2 Ack Phases.
@@ -615,6 +630,10 @@ fn rate_kbps(rate: u64) -> isize {
     } else {
         (rate * 8 / 1000) as isize
     }
+}
+
+fn state_str(r: &Congestion, _now: Instant) -> &'static str {
+    r.bbr2_state.state.into()
 }
 
 fn debug_fmt(r: &Congestion, f: &mut std::fmt::Formatter) -> std::fmt::Result {

quiche/src/recovery/gcongestion/bbr2/probe_bw.rs
@@ -62,6 +62,16 @@ enum AdaptUpperBoundsResult {
 }
 
 impl ModeImpl for ProbeBW {
+    fn state_str(&self) -> &'static str {
+        match self.cycle.phase {
+            CyclePhase::NotStarted => unreachable!(),
+            CyclePhase::Up => "bbr_probe_bw_up",
+            CyclePhase::Down => "bbr_probe_bw_down",
+            CyclePhase::Cruise => "bbr_probe_bw_cruise",
+            CyclePhase::Refill => "bbr_probe_bw_refill",
+        }
+    }
+
     fn enter(
         &mut self, now: Instant,
         _congestion_event: Option<&BBRv2CongestionEvent>, params: &Params,