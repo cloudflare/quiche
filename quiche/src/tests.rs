32f24308f0d0514fd60a7493325401100e3c3046
Will Hawkins <hawkinsw@obs.cr>
2025-10-08 18:33:07 -0400

    Decode Version Information Transport Parameter

--- quiche/src/tests.rs
+++ quiche/src/tests.rs
@@ -51,6 +51,7 @@ fn transport_params() {
         initial_source_connection_id: Some(b"woot woot".to_vec().into()),
         retry_source_connection_id: Some(b"retry".to_vec().into()),
         max_datagram_frame_size: Some(32),
+        version_information: None,
         unknown_params: Default::default(),
     };
 
@@ -81,6 +82,7 @@ fn transport_params() {
         initial_source_connection_id: Some(b"woot woot".to_vec().into()),
         retry_source_connection_id: None,
         max_datagram_frame_size: Some(32),
+        version_information: None,
         unknown_params: Default::default(),
     };
 
@@ -92,6 +94,38 @@ fn transport_params() {
     let new_tp = TransportParams::decode(raw_params, true, None).unwrap();
 
     assert_eq!(new_tp, tp);
+}
+
+#[test]
+fn transport_params_decode_version_information() {
+    // Given an encoded version_information transport parameter, ...
+    let raw_version_information_params = [
+        0x11, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0xfa, 0x5a,
+        0x0a, 0x7a, 0xff, 0x00, 0x00, 0x1d,
+    ];
+    let decoded_version_information_params = TransportParams::decode(
+        &raw_version_information_params,
+        true,
+        None,
+    )
+    .expect(
+        "Could not decode transport parameters with just version information.",
+    );
+
+    // ... make sure that it parses to what is expected.
+
+    let VersionNegotiationParameter {
+        chosen_version,
+        available_versions,
+    } = decoded_version_information_params
+        .version_information
+        .expect(
+            "Could not find the decoded version_information transport param.",
+        );
+    assert_eq!(chosen_version, 1u32);
+
+    let available_versions = available_versions.expect("Could not get the available versions from the decoded version_information transport param.");
+    assert_eq!(available_versions, [1, 0xfa5a0a7a, 0xff00001d]);
 }
 
 #[test]
