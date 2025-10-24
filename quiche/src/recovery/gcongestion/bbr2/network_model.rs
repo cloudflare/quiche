81237ce5f4ba4f21da9485e0a1a6fe7f8c4a2d8b
Antonio Vicente <avicente@cloudflare.com>
2025-10-24 12:23:17 -0700

    Only check for bandwidth growth in cases where last_packet_send_state is valid

--- quiche/src/recovery/gcongestion/bbr2/network_model.rs
+++ quiche/src/recovery/gcongestion/bbr2/network_model.rs
@@ -618,6 +618,10 @@ impl BBRv2NetworkModel {
             return true;
         }
 
+        if !congestion_event.last_packet_send_state.is_valid {
+            return false;
+        }
+
         let ignore_round = self.ignore_app_limited_for_no_bandwidth_growth &&
             congestion_event.last_packet_send_state.is_app_limited;
 
