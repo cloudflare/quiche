4c7bd2d49291057cda254f75301dd61180d3f494
Ted <thatree.ludwig@gmail.com>
2025-10-31 14:50:11 +0100

    fix: zero-init msghdr before field assignment (musl)

--- datagram-socket/src/mmsg.rs
+++ datagram-socket/src/mmsg.rs
@@ -25,9 +25,7 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 use std::io::IoSlice;
-use std::io::{
-    self,
-};
+use std::io::{self};
 use std::os::fd::AsRawFd;
 use std::os::fd::BorrowedFd;
 
@@ -55,16 +53,17 @@ pub fn recvmmsg(fd: BorrowedFd, bufs: &mut [ReadBuf<'_>]) -> io::Result<usize> {
 
             slices.push(IoSlice::new(b));
 
+            let mut msg_hdr: libc::msghdr = unsafe { std::mem::zeroed() };
+            msg_hdr.msg_name = std::ptr::null_mut();
+            msg_hdr.msg_namelen = 0;
+            msg_hdr.msg_iov = slices.last_mut().unwrap() as *mut _ as *mut _;
+            msg_hdr.msg_iovlen = 1;
+            msg_hdr.msg_control = std::ptr::null_mut();
+            msg_hdr.msg_controllen = 0;
+            msg_hdr.msg_flags = 0;
+
             msgvec.push(libc::mmsghdr {
-                msg_hdr: libc::msghdr {
-                    msg_name: std::ptr::null_mut(),
-                    msg_namelen: 0,
-                    msg_iov: slices.last_mut().unwrap() as *mut _ as *mut _,
-                    msg_iovlen: 1,
-                    msg_control: std::ptr::null_mut(),
-                    msg_controllen: 0,
-                    msg_flags: 0,
-                },
+                msg_hdr,
                 msg_len: buf.capacity().try_into().unwrap(),
             });
         }
@@ -115,16 +114,17 @@ pub fn sendmmsg(fd: BorrowedFd, bufs: &[ReadBuf<'_>]) -> io::Result<usize> {
         for buf in bufs.iter() {
             slices.push(IoSlice::new(buf.filled()));
 
+            let mut msg_hdr: libc::msghdr = unsafe { std::mem::zeroed() };
+            msg_hdr.msg_name = std::ptr::null_mut();
+            msg_hdr.msg_namelen = 0;
+            msg_hdr.msg_iov = slices.last_mut().unwrap() as *mut _ as *mut _;
+            msg_hdr.msg_iovlen = 1;
+            msg_hdr.msg_control = std::ptr::null_mut();
+            msg_hdr.msg_controllen = 0;
+            msg_hdr.msg_flags = 0;
+
             msgvec.push(libc::mmsghdr {
-                msg_hdr: libc::msghdr {
-                    msg_name: std::ptr::null_mut(),
-                    msg_namelen: 0,
-                    msg_iov: slices.last_mut().unwrap() as *mut _ as *mut _,
-                    msg_iovlen: 1,
-                    msg_control: std::ptr::null_mut(),
-                    msg_controllen: 0,
-                    msg_flags: 0,
-                },
+                msg_hdr,
                 msg_len: buf.capacity().try_into().unwrap(),
             });
         }
