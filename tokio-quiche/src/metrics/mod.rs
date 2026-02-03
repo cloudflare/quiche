eac4abd7072ebfe55610fed0db64cbf313e158a6
toidiu <apoorv@toidiu.com>
2026-02-03 02:41:41 -0800

    tokio-quiche: add metric to track flush aborts due to the future being cancelled
    This adds a reusable Token to track when an operation is complete or
    report the failure via a metric. The flush operation is part of a
    `select!` call and can therefore be cancelled if another future
    completes first. We want to track how often this happens.

--- tokio-quiche/src/metrics/mod.rs
+++ tokio-quiche/src/metrics/mod.rs
@@ -58,6 +58,10 @@ pub trait Metrics: Send + Sync + Clone + Unpin + 'static {
     /// WouldBlock and are retried in a loop.
     fn send_to_wouldblock_duration_s(&self) -> TimeHistogram;
 
+    /// Number of mid-handshake flush operations that were skipped due to future
+    /// cancellation.
+    fn skipped_mid_handshake_flush_count(&self) -> Counter;
+
     /// Number of QUIC packets received where the CID could not be verified.
     fn invalid_cid_packet_count(&self, reason: crate::BoxError) -> Counter;
 
@@ -159,6 +163,10 @@ impl Metrics for DefaultMetrics {
 
     fn send_to_wouldblock_duration_s(&self) -> TimeHistogram {
         quic::send_to_wouldblock_duration_s()
+    }
+
+    fn skipped_mid_handshake_flush_count(&self) -> Counter {
+        quic::skipped_mid_handshake_flush_count()
     }
 
     fn invalid_cid_packet_count(&self, reason: crate::BoxError) -> Counter {
@@ -276,6 +284,10 @@ pub(crate) mod quic {
     /// WouldBlock and are retried in a loop.
     #[ctor = HistogramBuilder { buckets: &[1E-6, 1E-5, 1E-4, 1E-3, 5E-3, 1E-2, 2E-2, 4E-2, 8E-2, 16E-2, 1.0], }]
     pub fn send_to_wouldblock_duration_s() -> TimeHistogram;
+
+    /// Number of mid-handshake flush operations that were skipped due to future
+    /// cancellation.
+    pub fn skipped_mid_handshake_flush_count() -> Counter;
 
     /// Number of QUIC packets received where the CID could not be verified.
     pub fn invalid_cid_packet_count(reason: String) -> Counter;
