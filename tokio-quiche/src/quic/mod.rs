399cfa9397050f3fc90b2d8dd546464e2256e7f8
Yi Huang <yi@cloudflare.com>
2026-03-27 18:29:25 -0700

    tokio-quiche: expose client provided DCID interface

--- tokio-quiche/src/quic/mod.rs
+++ tokio-quiche/src/quic/mod.rs
@@ -196,6 +196,27 @@ where
     let mut client_config = Config::new(params, socket.capabilities)?;
     let scid = SimpleConnectionIdGenerator.new_connection_id();
 
+    #[cfg(feature = "custom-client-dcid")]
+    let mut quiche_conn = if let Some(dcid) = &params.dcid {
+        quiche::connect_with_dcid_and_buffer_factory(
+            host,
+            &scid,
+            dcid,
+            socket.local_addr,
+            socket.peer_addr,
+            client_config.as_mut(),
+        )?
+    } else {
+        quiche::connect_with_buffer_factory(
+            host,
+            &scid,
+            socket.local_addr,
+            socket.peer_addr,
+            client_config.as_mut(),
+        )?
+    };
+
+    #[cfg(not(feature = "custom-client-dcid"))]
     let mut quiche_conn = quiche::connect_with_buffer_factory(
         host,
         &scid,
@@ -204,6 +225,9 @@ where
         client_config.as_mut(),
     )?;
 
+    #[cfg(feature = "custom-client-dcid")]
+    log::info!("created unestablished quiche::Connection"; "scid" => ?scid, "provided_dcid" => ?params.dcid);
+    #[cfg(not(feature = "custom-client-dcid"))]
     log::info!("created unestablished quiche::Connection"; "scid" => ?scid);
 
     if let Some(session) = &params.session {
